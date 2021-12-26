a5a99070aa48cf0c37c803aebff011f20019829d
Johannes <Lenk.Johannes@schueler.6072-fuerth.de>
2021-12-26 13:27:23 +0100

    css toggle

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,10 +9,15 @@ import { Hero} from "../hero";
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero? : Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
