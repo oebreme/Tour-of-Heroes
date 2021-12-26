bf6e6931394a7d3c0bf073c541a0352dcedefda6
Johannes <Lenk.Johannes@schueler.6072-fuerth.de>
2021-12-26 13:09:27 +0100

    added mock-heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: "Captain America" },
+  { id: 11, name: "Ironman" },
+  { id: 12, name: "Hulk" },
+  { id: 13, name: "Thor" },
+  { id: 14, name: "Black Widow" },
+  { id: 15, name: "Spiderman" },
+  { id: 16, name: "Hawkeye" },
+  { id: 17, name: "Dr. Strange" },
+  { id: 18, name: "Groot" },
+  { id: 19, name: "Black Panther" },
+  { id: 20, name: "Starlord" },
+  { id: 21, name: "Antman" },
+  { id: 22, name: "Nick Fury" },
+  { id: 23, name: "Captain Marvel" },
+  { id: 24, name: "Scarlet Witch" },
+  { id: 25, name: "Falcon" }
+  ]
