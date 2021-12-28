8349836408141fb0d053bde0809cbbdeb5b1acf8
Johannes <Lenk.Johannes@schueler.6072-fuerth.de>
2021-12-28 13:54:39 +0100

    tutorial concluded

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,32 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from "rxjs";
-import { Hero } from "../hero";
-import { HeroService} from "../hero.service";
+import { Observable, Subject } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 import { debounceTime, distinctUntilChanged, switchMap
-} from "rxjs/operators";
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  styleUrls: ['./hero-search.component.css']
+  styleUrls: [ './hero-search.component.css' ]
 })
 export class HeroSearchComponent implements OnInit {
   heroes$!: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
 
-  constructor(heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   search(term: string): void {
     this.searchTerms.next(term);
   }
 
   ngOnInit(): void {
-  this.heroes$ = this.searchTerms.pipe(
-    debounceTime(300),
-    distinctUntilChanged(),
-    switchMap((term: string) =>
-      this.heroService.searchHeroes(term)),
-  );
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
   }
-
 }
